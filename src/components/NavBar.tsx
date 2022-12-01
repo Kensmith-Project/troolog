56c8d379f7beb33fd7f740a9f04ab74f7df2aca9
Ajah Kenneth <kennethajah.u@gmail.com>
2022-12-01 16:26:13 +0100

    second-commit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { useRef } from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+import '../Stlyes/main.css'
+import useState from 'react'
+import troolog from '../assets/Troolog.svg'
 
 const NavBar = () => {
+   const [active, setActive] = React.useState("nav__menu");
+  const [icon, setIcon] = React.useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
   return (
-    <div>NavBar</div>
-  )
+    <nav className="nav">
+     <img src={troolog} alt='troolog' className='troolog-class'/>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+           About Us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+          Services
+          </a>
+        </li>
+        <li className="nav__item">
+          <button className='middle-box-inner-button-nav'>Sign in</button>
+        </li>
+        
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,13 +5,24 @@ import ibm from '../assets/ibm.svg'
 import meta from '../assets/meta.svg'
 import microsoft from '../assets/microsoft.svg'
 import slack from '../assets/slack.svg'
+import doteddone from '../assets/doteddone.svg'
+import itsupport from '../assets/itsupportImage.svg'
+import unsplash from '../assets/unsplash_Lks7vei-eAg (1).svg'
+import troolog from '../assets/Troolog.svg'
+import troolog2 from '../assets/troolog2.svg'
+import facebookicon from '../assets/facebook.svg'
+import linkedin from '../assets/linkedin.svg'
+import instagram from '../assets/instagram.svg'
+import NavBar from '../components/NavBar'
+
 
 const LandingPage = () => {
   return (
    <main>
+    <NavBar/>
     <section className='overall-session'>
         <div className='section-1'>
-            <div>
+            <div className="firstlayimg">
                 <img src={ladyicon} alt='lady'/>
             </div>
             <div className='section1-right'>
@@ -66,6 +77,119 @@ const LandingPage = () => {
            <iframe width="560" height="315" src="https://www.youtube.com/embed/08mV2SxRn1k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className='full-with'></iframe>
         </div>
     </section>
+    <div className="main-bottom">
+        <h1 className='for-business-text'>For Businesses</h1>
+        <h2 className='after-business-text'>Troolog enables businesses to gather internal feedbacks in a way that’s exremely seamless.</h2>
+        <div className="section-list-item">
+            <div className="container-of-list">
+               <ul>
+                 <li className="item-of-list">
+                 <img src={doteddone} alt='doteddone' className='dotted-done'/>
+                  <p>Gather feedback from employees seamlessly with no time wasted.
+</p>
+                </li>
+                 <li className="item-of-list">
+                 <img src={doteddone} alt='doteddone' className='dotted-done'/>
+                  <p>Troolog automatically applies analytics to group feedbacks based on sentiments, suggestions and other relevant categories.
+
+
+</p>
+                </li>
+                <li className="item-of-list">
+                 <img src={doteddone} alt='doteddone'className='dotted-done'/>
+                  <p>Use the feedback analytics report to synthesize meaning and conclusions.
+
+
+
+</p>
+                </li>
+                 <li className="item-of-list">
+                 <img src={doteddone} alt='doteddone' className='dotted-done'/>
+                  <p>Make presentations to wider teams with the analytics reports.
+
+
+
+</p>
+                </li>
+                 <li className="item-of-list">
+                 <img src={doteddone} alt='doteddone' className='dotted-done'/>
+                  <p>Bring your business closer to customers, sustain revenue.
+
+
+
+</p>
+                </li>
+                <button className='start-free-white-btn'>Start for free</button>
+               </ul>
+            </div>
+            <div className="image-right-side">
+    <img src={itsupport} alt='itsupport' className='itsupport'/>
+            </div>
+        </div>
+        
+
+    </div>
+
+    <div className='section-1'>
+            <div className='unpslash-box'>
+                <img src={unsplash} alt='lady' className='unpslash-box-image'/>
+            </div>
+            <div className='section1-right'>
+                <div className='middle-box2'>
+                    <div className='middle-box-inner2'>
+                        <h2 className='contact-us-text'>
+                   Contact us
+                </h2>
+                <p className='after-contact-text'>Sometimes you need a little help from your friends.  Or Troolog support rep. Don’t worry we’re here for you.</p>
+                <button className='open-mail-btn'>Open Mail</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer className='footer'>
+            <div className="social-icon-section">
+                <div className="troolog">
+                    <img src={troolog2} alt='trolog' className="trologimg"/>
+                </div>
+                <div className="facebook-svg-side">
+                    <img src={facebookicon} alt="facebook" className='fc-icon'/>
+                    <img src={linkedin} alt="linkedin" className='fc-icon'/>
+                    <img src={instagram} alt="instagram"className='fc-icon'/>
+
+                </div>
+                
+            </div>
+            <div className="footer-side-box">
+                <div className="first-footer-list">
+                    <ul>
+                        <li className='listheader'>
+                            Company
+                        </li>
+                        <li className='listafter'>
+                            Terms
+                        </li>
+                        <li className='listafter'>
+                            Privacy
+                        </li>
+                    </ul>
+
+                </div>
+                <div className="second-footer-list">
+                    <ul>
+                        <li className='listheader'>
+                           Contact
+                        </li>
+                        <li className='listafter'>
+                           Support@troolog.com
+                        </li>
+                        
+                    </ul>
+
+
+                </div>
+            </div>
+
+        </footer>
     
    </main>
   )
